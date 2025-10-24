9b0c8df22e1b86a18305a3a8bf24a3152486f3bd
Zahida222 <zahidazm-azmpa201@code.edu.az>
2025-10-24 21:31:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,55 +6,60 @@ namespace HomeTask2
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Admin məlumatlarını daxil edin.");
+{
+    Console.WriteLine("Admin məlumatlarını daxil edin.");
+    
 
-            string username;
-            while (true)
-            {
-                Console.Write("Username: ");
-                username = Console.ReadLine();
-                if (!string.IsNullOrEmpty(username) && username.Length > 6)
-                    break;
-                Console.WriteLine("Username minimum 7 simvoldan ibarət olmalıdır. ");
-            }
+    string username;
+    while (true)
+    {
+        Console.Write("Username: ");
+        username = Console.ReadLine();
+        if (User.IsValidUsername(username))
+            break;
+        Console.WriteLine("Username minimum 7 simvoldan ibarət olmalıdır. ");
+    }
 
-            string password;
-            while (true)
-            {
-                Console.Write("Password: ");
-                password = Console.ReadLine();
-                int upperCount = 0, digitCount = 0;
-                foreach (char c in password)
-                {
-                    if (char.IsUpper(c)) upperCount++;
-                    if (char.IsDigit(c)) digitCount++;
-                }
-                if (upperCount >= 1 && digitCount >= 1)
-                    break;
-                Console.WriteLine("Password ən azı 1 böyük hərf və 1 rəqəm daxil etməlidir.");
-            }
+    string password;
+    while (true)
+    {
+        Console.Write("Password: ");
+        password = Console.ReadLine();
+        if (User.IsValidPassword(password))
+            break;
+        Console.WriteLine("Password ən azı 1 böyük hərf və 1 rəqəm daxil etməlidir.");
+    }
 
-            string email;
-            while (true)
-            {
-                Console.Write("Email (istəyə bağlı, daxil etmək istəyirsinizsə @ olmalıdır): ");
-                email = Console.ReadLine();
-                if (string.IsNullOrEmpty(email) || (email.Contains("@") && !"!#$%&'*+/=?^_`{|}~.-".Contains(email[0]) && !"!#$%&'*+/=?^_`{|}~.-".Contains(email[email.Length - 1])))
-                    break;
-                Console.WriteLine("Email düzgün formatda deyil. Yenidən daxil edin.");
-            }
+    string email;
+    while (true)
+    {
+        Console.Write("Email (istəyə bağlı, daxil etmək istəyirsinizsə @ olmalıdır): ");
+        email = Console.ReadLine();
+        if (string.IsNullOrEmpty(email) || User.IsValidEmail(email)) 
+            break;
+        Console.WriteLine("Email düzgün formatda deyil. Yenidən daxil edin.");
+    }
 
-            bool isSuperAdmin = true;
-            string section = "AzMpa201"; 
+    bool isSuperAdmin = true;
 
-            Admin admin = Admin.CreateAdmin(username, password, section, isSuperAdmin, email);
+    string section;
+    while (true)
+    {
+        Console.Write("Section:");
+        section= Console.ReadLine();
+        if (!string.IsNullOrEmpty(section))
+            break;
+    }
 
-            Console.WriteLine("\nAdmin məlumatları uğurla yaradıldı:");
-            admin.DisplayAdminInfo();
+     
 
-            
-            
-        }
+    Admin admin = Admin.CreateAdmin(username, password, section, isSuperAdmin, email);
+
+    Console.WriteLine("\nAdmin məlumatları uğurla yaradıldı:");
+    admin.DisplayAdminInfo();
+
+    
+    
+}
     }
 }
